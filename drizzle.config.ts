4eb16efad97ae720a169282a397519948a06ad62
pengshixian <61853492+sx565355044@users.noreply.github.com>
2025-03-12 19:03:55 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,8 +7,8 @@ if (!process.env.DATABASE_URL) {
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
-  dialect: "postgresql",
+  dialect: "mysql2",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    uri: process.env.DATABASE_URL,
   },
 });
